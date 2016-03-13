8b8ab8d8f983da5211fe887aa4e1749a616294bb
Tommaso Bufano <tbufano@thinkseed.com>
2016-03-13 16:49:32 -0500

    added the friendly_updated_at, full_name, and japan_phone models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ActiveRecord::Base
+  def friendly_updated_at
+    updated_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def japan_phone
+    "+81 #{phone_number}"
+  end
 end
